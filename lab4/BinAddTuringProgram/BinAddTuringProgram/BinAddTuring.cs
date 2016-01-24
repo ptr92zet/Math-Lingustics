74f7fa55c621bbea26670728c3937983f54cce60
Piotr Zietek <zietekpiotr5@gmail.com>
2016-01-25 00:17:41 +0100

    BinAddTuring - lab4. Corrected small mistake - uncommented State enum! Needed to run code from previous commit!

--- lab4/BinAddTuringProgram/BinAddTuringProgram/BinAddTuring.cs
+++ lab4/BinAddTuringProgram/BinAddTuringProgram/BinAddTuring.cs
@@ -366,18 +366,18 @@ namespace BinAddTuringProgram
         }
     }
 
-    //public enum State
-    //{
-    //    Q0 = 0,
-    //    Q1,
-    //    Q2,
-    //    Q3,
-    //    Q4,
-    //    Q5,
-    //    Q6,
-    //    Q7,
-    //    Q8,
-    //    Q9,
-    //    QF
-    //}
+    public enum State
+    {
+        Q0 = 0,
+        Q1,
+        Q2,
+        Q3,
+        Q4,
+        Q5,
+        Q6,
+        Q7,
+        Q8,
+        Q9,
+        QF
+    }
 }
